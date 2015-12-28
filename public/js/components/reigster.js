56ebb3417e49e0dffc7800f74b13b9236b996d71
pengfeng <pengfeng91@163.com>
2015-12-28 18:39:29 +0800

    login and register

--- public/js/components/reigster.js
+++ public/js/components/reigster.js
@@ -1,24 +1,28 @@
-var React = window.React = require('react');
-var ReactDOM = window.ReactDOM = require('react-dom');
+/**
+ * Created by Christ on 2015/12/28.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
 var ajaxQuery = require('../util/ajaxQuery');
 var List = require('./list').List;
-var RegisterBox = require('./reigster').RegisterBox;
+var LoginBox1 = require('./login').LoginBox;
 
-var LoginBox = React.createClass({
+var RegisterBox = React.createClass({
     getInitialState: function () {
-        return {name: '', password: '', waringClass: 'lrHide', waringText: ''}
+        return {name: '', password: '', repassword: '',waringClass: 'lrHide', waringText: ''}
     },
     render: function() {
         var waringClass = this.state.waringClass + ' lrWaring';
         return (
-            <div id="loginBox" className="lrBox">
-                <div id="loginCover"></div>
+            <div id="registerBox" className="lrBox">
+                <div id="registerCover"></div>
                 <form>
                     <p><input type="text" onChange={this.handleChange().name} placeholder="账号" value={this.state.name}/></p>
                     <p><input type="password" onChange={this.handleChange().password} placeholder="密码" value={this.state.password} /></p>
+                    <p><input type="password" onChange={this.handleChange().repassword} placeholder="再次输入" value={this.state.repassword} /></p>
                     <p><span className={waringClass}>{this.state.waringText}</span></p>
-                    <p><input onClick={this.login} type="button" value="登录"/></p>
-                    <p><a onClick={this.gotoRegister} className="lrGoLoginOrRe">没有账号？注册→</a></p>
+                    <p><input onClick={this.register} type="button" value="注册"/></p>
+                    <p><a href="/" className="lrGoLoginOrRe">已有账号？登录→</a></p>
                 </form>
             </div>
         )
@@ -31,20 +35,31 @@ var LoginBox = React.createClass({
             },
             password: function(event) {
                 _this.setState({password: event.target.value});
+            },
+            repassword: function(event) {
+                _this.setState({repassword: event.target.value});
             }
         }
     },
-    login: function() {
+    register: function() {
         var _this = this;
+        console.log(this.state.name, this.state.password, this.state.repassword);
         if(this.state.name == '') {
             _this.setState({waringClass: 'lrShow', waringText: '账号不能为空'});
             return false;
         }
-        if(this.state.password == '') {
+        if(this.state.password == '' || this.state.repassword == '') {
             _this.setState({waringClass: 'lrShow', waringText: '密码不能为空'});
             return false;
         }
-        ajaxQuery.post('/user/login', {name: this.state.name, password: this.state.password}, function(msg) {
+        if(this.state.password != this.state.repassword) {
+            _this.setState({waringClass: 'lrShow', waringText: '两次密码不一致'});
+            return false;
+        }
+        var name = this.state.name;
+        var password = this.state.password;
+
+        ajaxQuery.post('/user/register', {name: name, password: password}, function(msg) {
             if(msg.code == 0) {
                 ajaxQuery.get('/article/getArticles', function(msg) {
                     _this.setState({waringClass: 'lrHide'});
@@ -52,22 +67,12 @@ var LoginBox = React.createClass({
                         <List articles={msg.article}></List>
                         ,document.getElementById('mainWrap')
                     );
-                    console.log(msg);
                 })
             }else{
                 _this.setState({waringClass: 'lrShow', waringText: msg.message});
             }
+            console.log(msg);
         });
-    },
-    gotoRegister: function() {
-        ReactDOM.render(
-            <RegisterBox></RegisterBox>
-            ,document.getElementById('mainWrap')
-        );
     }
 });
-
-exports.LoginBox = LoginBox;
-
-
-
+exports.RegisterBox = RegisterBox;
