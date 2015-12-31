78309c405ee1b1054cc1110479be85f4add87db8
pengfeng <pengfeng91@163.com>
2015-12-31 22:57:26 +0800

    kuayu

--- app.js
+++ app.js
@@ -25,6 +25,15 @@ require("babel-core/register");
 //});
 
 var app = express();
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
 app.use(cookieParser());
 app.use(mps());
 app.set('views', path.join(__dirname, 'views'));
