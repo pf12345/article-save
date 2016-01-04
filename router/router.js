42906437ba06806d85d013f29d1c98c45b4195fc
pengfeng <pengfeng91@163.com>
2016-01-04 11:35:27 +0800

    mobile develop

--- router/router.js
+++ router/router.js
@@ -5,7 +5,7 @@ var user = require('../controllers/user');
 var article = require('../controllers/article');
 exports.init = function(app) {
     app.get('/',function(req, res, next) {
-        res.setHeader("Content-Type", "text/plain");
+        res.setHeader("Content-Type", "text/html");
         res.render('home/index', {});
     });
     user.router(app);
